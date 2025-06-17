214db0ad75e97220ea2d3923b26471ec2d2236e9
Laura <lauraamperin@gmail.com>
2025-06-17 12:19:21 -0300

    listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -20,10 +20,10 @@ Este projeto tem como objetivo integrar análise de dados contábeis de empresas
 import streamlit as st
 
 st.set_page_config(
-    page_title="Lista de Exercícios 4",
-    header="Projeto Final – Análise Contábil com Ajuste Econômico",
-    write="Este projeto tem como objetivo integrar análise de dados contábeis de empresas com indicadores econômicos, utilizando Python, Pandas, Ipeadata e Streamlit."
+    page_title="Lista de Exercícios 4"
 )
+st.header="Projeto Final – Análise Contábil com Ajuste Econômico",
+st.write="Este projeto tem como objetivo integrar análise de dados contábeis de empresas com indicadores econômicos, utilizando Python, Pandas, Ipeadata e Streamlit."
 
 """2) Importe os dados do arquivo empresas_dados.csv utilizando pandas e apresente todas as linhas da df (peso: 1,0)
 
